c0c3dd89f4be52d79a3a810e63e79ed9f3b0e00d
Colin Gleason <cjgleason@umass.edu>
2022-10-27 11:41:45 -0400

    Update check_results.R

--- R code/check_results.R
+++ R code/check_results.R
@@ -8,3 +8,7 @@ for (i in 1:length(list.files(QA_QC_drift_output_directory))){
 drift_test=readRDS(paste0(QA_QC_drift_output_directory,list.files(QA_QC_drift_output_directory)[i]))
 windows()
 plot(drift_test$GNSS_time_UTC,drift_test$GNSS_wse)}
+
+for (i in 1:length(list.files('Willamette/SWORD products/reach/'))){
+  reach_test_test=readRDS(paste0('Willamette/SWORD products/reach/',list.files('Willamette/SWORD products/reach/')[i]))
+}
