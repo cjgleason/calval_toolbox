18dee4e90da2e2b6a8ea03b9b351fcfb5d92f701
cjgleason <cjgleason@umass.edu>
2022-12-23 10:20:49 -0500

    Update profile_analysis.R

--- R code/profile_analysis.R
+++ R code/profile_analysis.R
@@ -1,8 +1,9 @@
-setwd('D:\\OneDrive -\ University of Massachusetts\\calval\\Toolbox\\calval_toolbox\\Willamette\\')
+#setwd('D:\\OneDrive -\ University of Massachusetts\\calval\\Toolbox\\calval_toolbox\\Willamette\\')
+setwd('C:\\Users\\colin\\Documents\\GitHub\\calval_toolbox\\Willamette\\')
 library(ncdf4)
 
 
-test=nc_open('D:\\OneDrive -\ University of Massachusetts\\calval\\Toolbox\\calval_toolbox\\Willamette\\rivertile.nc')
+test=nc_open('rivertile.nc')
 
 #names(test$var)
 
@@ -23,7 +24,7 @@ nc_close(test)
 
 #sample 5 nodes
 point1=8
-point5=(length(node_ids)-2)
+point5=(length(node_ids))
 point2=sample(20:30,1)
 point3=sample(45:55,1)
 point4=sample(70:80,1)
@@ -45,12 +46,12 @@ points(c(node_ids[point1],
            this_node_wse[point2],
            this_node_wse[point3],
            this_node_wse[point4],
-           this_node_wse[point5] )  ,col='red'  ,lwd=10   )
+           this_node_wse[point5] )  ,col='black'  ,lwd=10   )
 text(node_ids[point2],this_node_wse[point5] ,paste0('true mean: ',round(average_height_true,digits=3), ' m'))
 text(node_ids[point2],this_node_wse[point4] ,paste0('PT mean: ',round(average_height_sample,digits=3), ' m'))
 
 #now perturb the profile
-this_node_wse_new = this_node_wse - 1 - runif(length(this_node_wse),0.0,0.05)
+this_node_wse_new = this_node_wse - 1 - runif(length(this_node_wse),0.0,0.10)
 
 true_delta= mean(this_node_wse,na.rm=T)- mean(this_node_wse_new,na.rm=T)
 sample_delta= mean(    c(this_node_wse[point1],
@@ -77,4 +78,16 @@ points(c(node_ids[point1],
          this_node_wse_new[point5] )  ,col='blue'  ,lwd=10   )
 text(node_ids[point4],this_node_wse[point2] ,paste0('true delta: ',round(true_delta,digits=3), ' m'))
 text(node_ids[point4],this_node_wse[point1] ,paste0('PT delta: ',round(sample_delta,digits=3), ' m'))
+
+points(c(node_ids[point1],
+         node_ids[point2],
+         node_ids[point3],
+         node_ids[point4],
+         node_ids[point5]),
+       c(this_node_wse_new[point1]+runif(1,3,3.3),
+         this_node_wse_new[point2]+runif(1,3,3.3),
+         this_node_wse_new[point3]+runif(1,3,3.3),
+         this_node_wse_new[point4]+runif(1,3,3.3),
+         this_node_wse_new[point5]+runif(1,3,3.3))  ,col='green'  ,lwd=10   )
+
 
